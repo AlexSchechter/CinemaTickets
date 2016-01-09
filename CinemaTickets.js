012b62805638a05992edcc06c936196fe50e4d83
AlexSchechter <a.schechter@outlook.com>
2016-01-09 17:15:20 -0500

    Minor Refactoring

--- CinemaTickets.js
+++ CinemaTickets.js
@@ -9,21 +9,6 @@ $(function(){
 		var price3 = $("#thirdPrice").text().substr(1);
 		$("#total").html("£" + (totalPerItem(price1, quantity1) + totalPerItem(price2, quantity2) + totalPerItem(price3, quantity3)).toFixed(2));
 	});
-	$("#p2").mouseenter(function(){
-		$(this).hide();
-	});
-	$("#p1").click(function(){
-		$(this).hide();
-	});
-	$("#p3").mousedown(function(){
-		alert("I told you so");
-	});
-	$("#myButton").click(function(){
-		$("#p4").toggle(1000);
-	});
-	$("#secondButton").click(function(){
-		$("#p5").animate({'left': '250px', 'top':'100px'}, "slow")
-	});
 })
 
 function totalPerItem(price, quantity){
